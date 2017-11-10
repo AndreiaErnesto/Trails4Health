eba00a1789df41bd5dcc2bc350977912e16883d6
janilta <32763796+janilta@users.noreply.github.com>
2017-11-10 12:23:51 +0000

    Branch

---
+++ ProjetoTrails4Health/Models/AgendarPercurso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoTrails4Health.Models
+{
+    public class AgendarPercurso
+    {
+        public int dataInicio { get; set; }
+        public int dataFim { get; set; }
+
+    }
+}
