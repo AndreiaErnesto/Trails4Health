b44bb4ea9691970217cfb9ffe6ed41fe388e6ee8
janilta <32763796+janilta@users.noreply.github.com>
2017-11-22 20:41:31 +0000

    Conexão a base de dados

---
+++ ProjetoTrails4Health/Data/AgendarTrilhoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoTrails4Health.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoTrails4Health.Data
+{
+    public class AgendarTrilhoDbContext :DbContext
+    {
+         public AgendarTrilhoDbContext(
+            DbContextOptions<AgendarTrilhoDbContext> options) : base(options) {
+        }
+        public DbSet<Agendar> AgendaTrilho { get; set; }
+    }
+}
