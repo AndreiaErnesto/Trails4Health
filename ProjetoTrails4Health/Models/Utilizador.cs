acb0bf3418f0c7f06430a46fe739a9da1adbb01c
AndreiaErnesto <32199098+AndreiaErnesto@users.noreply.github.com>
2017-11-22 15:02:20 +0000

    Criação da classe utilizador

---
+++ ProjetoTrails4Health/Models/Utilizador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoTrails4Health.Models
+{
+    public class Utilizador{
+        public int ID_Utilizador { get; set; }
+        public string Nome { get; set; }
+        public string Password { get; set; }
+        public string Morada { get; set; }
+        public string CodPostal { get; set; }
+        public string Email { get; set; }
+        public string Telemovel { get; set; }
+        public string DataNascimento { get; set; }
+        public string NIF { get; set; }
+    }
+}
