d29ea40403d8916e0cbeb4de1a4a375c732dd2cd
AndreiaErnesto <32199098+AndreiaErnesto@users.noreply.github.com>
2017-11-22 15:06:01 +0000

    Criação DbContext dos utilizador

---
+++ ProjetoTrails4Health/Data/UtilizadorDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace ProjetoTrails4Health.Data
+{
+    public class UtilizadorDbContext : DbContext
+    {
+        public UtilizadorDbContext(
+            DbContextOptions<UtilizadorDbContext> options) : base(options){
+        }
+
+        //DbSet para todas as classes
+        public DbSet<Models.Utilizador> Utilizadores;
+        
+    }
+}
