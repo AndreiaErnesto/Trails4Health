1b0ddf48a8a15f945ec68ae9ca4e92c988a619ae
AndreiaErnesto <32199098+AndreiaErnesto@users.noreply.github.com>
2017-11-23 18:12:27 +0000

    Alterações

--- ProjetoTrails4Health/Data/SeedData.cs
+++ ProjetoTrails4Health/Data/SeedData.cs
@@ -43,11 +43,11 @@ namespace ProjetoTrails4Health.Data
         {
             dbContext.Turistas.AddRange(
                 turista1 = new Turista { Nome = "Pedro", Password = "PedroSanches21", Morada = "Rua Salgado n15", CodPostal = "2680-104", Email = "pedro@gmail.com", Telemovel = "914009711", DataNascimento = "22-11-1996", NIF = "987654321" },
-                turista1 = new Turista { Nome = "Andreia", Password = "AndreiaErnesto21", Morada = "Rua Ernesto n1", CodPostal = "6290-081", Email = "andreia@gmail.com", Telemovel = "965874555", DataNascimento = "03-12-1990", NIF = "234567889" },
-                turista1 = new Turista { Nome = "Maria", Password = "Maria21", Morada = "Rua Outeiro n1", CodPostal = "2586-100", Email = "maria@gmail.com", Telemovel = "912233344", DataNascimento = "28-02-1970", NIF = "589455366" },
-                turista1 = new Turista { Nome = "João", Password = "Joao21", Morada = "Rua Franciso Sá Carneiro n3", CodPostal = "1566-104", Email = "joao@gmail.com", Telemovel = "912358459", DataNascimento = "01-03-1996", NIF = "987654321" },
-                turista1 = new Turista { Nome = "Telmo", Password = "Telmo21", Morada = "Travessa n2", CodPostal = "7589-036", Email = "telmo@gmail.com", Telemovel = "965423568", DataNascimento = "12-12-1992", NIF = "123456789" },
-                turista1 = new Turista { Nome = "Rodrigo", Password = "Rodrigo21", Morada = "Rua Outeiro n1", CodPostal = "1445-100", Email = "rodrigo@gmail.com", Telemovel = "935789456", DataNascimento = "28-02-1978", NIF = "568123456" }
+                turista2 = new Turista { Nome = "Andreia", Password = "AndreiaErnesto21", Morada = "Rua Ernesto n1", CodPostal = "6290-081", Email = "andreia@gmail.com", Telemovel = "965874555", DataNascimento = "03-12-1990", NIF = "234567889" },
+                turista3 = new Turista { Nome = "Maria", Password = "Maria21", Morada = "Rua Outeiro n1", CodPostal = "2586-100", Email = "maria@gmail.com", Telemovel = "912233344", DataNascimento = "28-02-1970", NIF = "589455366" },
+                turista4 = new Turista { Nome = "João", Password = "Joao21", Morada = "Rua Franciso Sá Carneiro n3", CodPostal = "1566-104", Email = "joao@gmail.com", Telemovel = "912358459", DataNascimento = "01-03-1996", NIF = "987654321" },
+                turista5 = new Turista { Nome = "Telmo", Password = "Telmo21", Morada = "Travessa n2", CodPostal = "7589-036", Email = "telmo@gmail.com", Telemovel = "965423568", DataNascimento = "12-12-1992", NIF = "123456789" },
+                turista6 = new Turista { Nome = "Rodrigo", Password = "Rodrigo21", Morada = "Rua Outeiro n1", CodPostal = "1445-100", Email = "rodrigo@gmail.com", Telemovel = "935789456", DataNascimento = "28-02-1978", NIF = "568123456" }
             );
         }
 
