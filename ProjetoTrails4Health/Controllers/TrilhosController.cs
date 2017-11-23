1b88400621c9032240bf4f4c8f5c42c34413bc54
AndreiaErnesto <32199098+AndreiaErnesto@users.noreply.github.com>
2017-11-23 18:18:07 +0000

    Scaffolding Trilhos

---
+++ ProjetoTrails4Health/Controllers/TrilhosController.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetoTrails4Health.Data;
+using ProjetoTrails4Health.Models;
+
+namespace ProjetoTrails4Health.Controllers
+{
+    public class TrilhosController : Controller
+    {
+        private readonly AplicacaoDbContext _context;
+
+        public TrilhosController(AplicacaoDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Trilhoes
+        public async Task<IActionResult> Index()
+        {
+            var aplicacaoDbContext = _context.Trilho.Include(t => t.Dificuldade).Include(t => t.Professor);
+            return View(await aplicacaoDbContext.ToListAsync());
+        }
+
+        // GET: Trilhoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trilho = await _context.Trilho
+                .Include(t => t.Dificuldade)
+                .Include(t => t.Professor)
+                .SingleOrDefaultAsync(m => m.TrilhoId == id);
+            if (trilho == null)
+            {
+                return NotFound();
+            }
+
+            return View(trilho);
+        }
+
+        // GET: Trilhoes/Create
+        public IActionResult Create()
+        {
+            ViewData["DificuldadeId"] = new SelectList(_context.Set<Dificuldade>(), "DificuldadeId", "DificuldadeId");
+            ViewData["ProfessorId"] = new SelectList(_context.Professor, "ProfessorId", "ProfessorId");
+            return View();
+        }
+
+        // POST: Trilhoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TrilhoId,Nome_Trilho,Local_Inicio_Trilho,Local_Fim_Trilho,Distancia_Total,Duracao_Media,Esta_Ativo,Tempo_Gasto,ProfessorId,DificuldadeId")] Trilho trilho)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trilho);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["DificuldadeId"] = new SelectList(_context.Set<Dificuldade>(), "DificuldadeId", "DificuldadeId", trilho.DificuldadeId);
+            ViewData["ProfessorId"] = new SelectList(_context.Professor, "ProfessorId", "ProfessorId", trilho.ProfessorId);
+            return View(trilho);
+        }
+
+        // GET: Trilhoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trilho = await _context.Trilho.SingleOrDefaultAsync(m => m.TrilhoId == id);
+            if (trilho == null)
+            {
+                return NotFound();
+            }
+            ViewData["DificuldadeId"] = new SelectList(_context.Set<Dificuldade>(), "DificuldadeId", "DificuldadeId", trilho.DificuldadeId);
+            ViewData["ProfessorId"] = new SelectList(_context.Professor, "ProfessorId", "ProfessorId", trilho.ProfessorId);
+            return View(trilho);
+        }
+
+        // POST: Trilhoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TrilhoId,Nome_Trilho,Local_Inicio_Trilho,Local_Fim_Trilho,Distancia_Total,Duracao_Media,Esta_Ativo,Tempo_Gasto,ProfessorId,DificuldadeId")] Trilho trilho)
+        {
+            if (id != trilho.TrilhoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(trilho);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TrilhoExists(trilho.TrilhoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["DificuldadeId"] = new SelectList(_context.Set<Dificuldade>(), "DificuldadeId", "DificuldadeId", trilho.DificuldadeId);
+            ViewData["ProfessorId"] = new SelectList(_context.Professor, "ProfessorId", "ProfessorId", trilho.ProfessorId);
+            return View(trilho);
+        }
+
+        // GET: Trilhoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trilho = await _context.Trilho
+                .Include(t => t.Dificuldade)
+                .Include(t => t.Professor)
+                .SingleOrDefaultAsync(m => m.TrilhoId == id);
+            if (trilho == null)
+            {
+                return NotFound();
+            }
+
+            return View(trilho);
+        }
+
+        // POST: Trilhoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var trilho = await _context.Trilho.SingleOrDefaultAsync(m => m.TrilhoId == id);
+            _context.Trilho.Remove(trilho);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool TrilhoExists(int id)
+        {
+            return _context.Trilho.Any(e => e.TrilhoId == id);
+        }
+    }
+}
