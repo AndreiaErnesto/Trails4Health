f162ef176263be60c1fba1cd4e0131ce077ccaaa
janilta <32763796+janilta@users.noreply.github.com>
2017-12-07 00:06:19 +0000

    Validação do Formulário
    Validação do Formulário
    Alteração dos nomes no index da view AgendaTuristaTrilho

--- ProjetoTrails4Health/Models/Agenda_Turista_Trilho.cs
+++ ProjetoTrails4Health/Models/Agenda_Turista_Trilho.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,10 +14,14 @@ namespace ProjetoTrails4Health.Models
         public int TrilhoId { get; set; }
         public int Tempo_Gasto { get; set; }
         public Turista Turista { get; set; }
+        [Required(ErrorMessage = "Por favor introduza o nome do Turista")]
         public int TuristaId { get; set; }
 
         public DateTime Data_Reserva { get; set; }
+        [Required(ErrorMessage = "Por favor digite a data.")]
+        [RegularExpression(@"[\d\/]+", ErrorMessage = "Data Inválida")]
         public DateTime Data_Prevista_Inicio_Trilho { get; set; }
+        [Required(ErrorMessage = "Por favor introduza o estado do Agendamento do Trilho")]
         public string  Estado_Agendamento { get; set; }
         public DateTime Data_Estado_Agendamento { get; set; }
     }
